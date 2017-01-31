8c3b304a11c92de25d0ac9405a2849eed17b7ecd
Christian von der Weth <vonderweth@nus.edu.sg>
2017-01-31 10:13:06 +0800

    Cleared configuration

--- nodejs-server/config.js
+++ nodejs-server/config.js
@@ -1,18 +1,18 @@
 module.exports = {
   'db' : {
     'data': {
-      'host': '172.29.33.45',
-      'dbname': 'onespace',
-      'user': 'onespace',
-      'password': '!!5656tT'
+      'host': '<ip-or-hostname>',
+      'dbname': '<database-name>',
+      'user': '<database-username>',
+      'password': '<database-password>'
     },
     'ejabberd': {
-        'host': '172.29.33.45',
-        'dbname': 'ejabberd',
-        'user': 'ejabberd',
-        'password': '!!5656tT'
+        'host': '<ip-or-hostname>',
+        'dbname': '<database-name>',
+        'user': '<database-username>',
+        'password': '<database-password>'
      },
   },
-  'http-port': 11090,
-  'fcm-server-key': 'AAAARWQ_l9s:APA91bHXlgitEb7IxE3Agj424tiMKK0jof82HM8NrJfF-R220Hg_T8kK9UuouM0I74ixv7SFZnrfQ1yes_y5orM4Uf1LUWA5kJ3UgJwskhZeKLHNja88sWe4iNKPovA0ao4dWAFKe7Uu'
+  'http-port': <port>,
+  'fcm-server-key': '<fcm-server-key-as-provided-by-registered-fcm-application>'
 }; 
